2e77090d323b27eab34ac448f9f79aa709f017d4
shlomo <s0556732899@gmail.com>
2025-05-22 14:56:52 +0300

    aman

---
+++ IDF/Aman.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+public class Aman : Hamas
+{
+    Terorist name =;
+    string time;
+    string place;
+    List<string> list_message = new List<string>();
+    public Aman()
+    {
+        //this.name = name;
+        this.time = time;
+        this.place = place;
+        message();
+    }
+   
+    public void message() 
+        {
+        string msg = $"Report that {name} was in {place} at {time}";
+        list_message.Add(msg);
+        }  
+    }
+
+
+
--- IDF/Drone.cs
+++ IDF/Drone.cs
@@ -1,8 +1,9 @@
-﻿public class  Drone : AttackTool
-{
-    public Drone(string bombType)
-        : base("Drone",2, bombType,1,3)
+﻿
+    public class Drone : AttackTool
     {
+        public Drone(string bombType)
+            : base("Drone", 2, bombType, 1, 3)
+        {
 
+        }
     }
-}
